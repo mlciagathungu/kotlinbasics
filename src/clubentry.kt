6ae3e95851c09f4101365c93b2934a95f37e1c29
mlciagathungu <mlciagathungu1022@gmail.com>
2025-04-03 14:39:19 +0300

    clubentry.kt

--- src/clubentry.kt
+++ src/clubentry.kt
@@ -1,12 +1,26 @@
-fun main(){
-    println("Welcome to the club entry system")
-    print("Enter your age: ")
+
+fun main() {
+    println("Welcome to the club entry system.")
+    println("Enter your age")
     val age = readln().toIntOrNull() ?: 0
-    print("Do you have a VIP pass(yes/no) ")
-    val hasVipPass = readln().equals("yes" , ignoreCase = true)
-    print("Are you a member? (yes/no): ")
-    val isMember = readln().equals("yes" , ignoreCase = true)
-    println(age)
-    println(hasVipPass)
-    println(isMember)
+
+    when {
+        age <= 18 -> {
+            println("Sorry, your club entry request has been rejected !")
+        }
+        else -> {
+            println("congrats, your club entry request has been granted. ")
+            println("Are you a member or a VIP?")
+            val isMember = readln().equals("yes", ignoreCase = true)
+            print("Enter your member number:")
+            val memberNumber = readln()
+            println("Drinks discount applied to,member no $memberNumber!")
+            val isVip = readln().equals("yes", ignoreCase = true)
+            println("Welcome to the VIP lounge!")
+            println("Enjoy your night ")
+            println(age)
+            println(isVip)
+            println(isMember)
+        }
+    }
 }
